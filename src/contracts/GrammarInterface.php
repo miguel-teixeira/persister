74a35a25edb129b38f76e968724d5685ed5ea46a
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-12-31 16:16:54 +0000

    minimal interfaca

--- src/contracts/GrammarInterface.php
+++ src/contracts/GrammarInterface.php
@@ -4,11 +4,9 @@ namespace Persister\contracts;
 
 interface GrammarInterface
 {
-    public function compileExists(Record $record, $uid);
-
     public function compileSelectTableRows($tableName, $keyColumns, $keys);
 
     public function compileUpdate(Record $record);
 
-    public function compileInsert(Record $record);
+    public function compileInsertRows(array $records, array $recordsColumns);
 }
